ad9a8044435252510bb5a08aca22984e74b72704
Alexis.Avril <alexisavril@sfr.fr>
2025-12-04 19:41:35 +0100

    Adding animations and images to Header

--- my-portfolio/app/components/Header.tsx
+++ my-portfolio/app/components/Header.tsx
@@ -1,14 +1,24 @@
+import Image from "next/image";
+
 export default function Header() {
    return (
     <header id="header" className="w-full fixed top-0 left-0 bg-white shadow-md z-50">
       <nav className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         <div className="text-xl font-bold">Alexis AVRIL</div>
-        <ul className="flex space-x-6 justify-end">
-          <li><a href="#banner" className="hover:underline">Accueil</a></li>
-          <li><a href="#general-skills" className="hover:underline">Compétences</a></li>
-          <li><a href="#about" className="hover:underline">À propos</a></li>
-          <li><a href="#projects" className="hover:underline">Projets</a></li>
-          <li><a href="#contact" className="hover:underline">Contact</a></li>
+
+         <div className="flex space-x-2 mx-6">
+          <Image src="/spade.webp" alt="Pique" width={24} height={24} className="header-image" />
+          <Image src="/heart.webp" alt="Cœur" width={24} height={24} className="header-image" />
+          <Image src="/club.webp" alt="Trèfle" width={24} height={24} className="header-image" />
+          <Image src="/diamond.webp" alt="Carreau" width={24} height={24} className="header-image" />
+        </div>
+
+         <ul className="flex space-x-6 justify-end">
+          <li><a href="#banner" className="text-black hover:text-red-600 transition-colors duration-300">Accueil</a></li>
+          <li><a href="#general-skills" className="text-black hover:text-red-600 transition-colors duration-300">Compétences</a></li>
+          <li><a href="#about" className="text-black hover:text-red-600 transition-colors duration-300">À propos</a></li>
+          <li><a href="#projects" className="text-black hover:text-red-600 transition-colors duration-300">Projets</a></li>
+          <li><a href="#contact" className="text-black hover:text-red-600 transition-colors duration-300">Contact</a></li>
         </ul>
       </nav>
     </header>
