c18e5eda8711d5833cd6c1a7298ace74107b652f
Alexis.Avril <alexisavril@sfr.fr>
2025-12-05 05:57:03 +0100

    Work on GeneralSkills

--- my-portfolio/app/components/GeneralSkills.tsx
+++ my-portfolio/app/components/GeneralSkills.tsx
@@ -1,21 +1,42 @@
+import Image from "next/image";
+
 interface Skill {
   title: string;
   description: string;
+  image: string;
 }
 
 const skills: Skill[] = [
   {
     title: "Développement de sites Web",
-    description: "Création de sites modernes et fonctionnels avec HTML, CSS, JS et frameworks."
+    description: "Création de sites modernes et fonctionnels avec HTML, CSS, JS et frameworks.",
+    image: "/wand.webp"
   },
   {
     title: "Optimisation SEO",
-    description: "Amélioration du référencement et de la visibilité des sites."
+    description: "Amélioration du référencement et de la visibilité des sites.",
+    image: "/rabbitHat.webp"
   },
   {
     title: "Responsive Design",
-    description: "Adaptation des sites pour tous types d’écrans et appareils."
-  }
+    description: "Adaptation des sites pour tous types d’écrans et appareils.",
+    image: "/stars.webp"
+  },
+  {
+    title: "Gestion de projets",
+    description: "Planification et coordination efficaces pour respecter les délais.",
+    image: "/crystalBall.webp"
+  },
+  {
+    title: "Maintenance de sites Web",
+    description: "Mises à jour régulières et résolution de problèmes techniques.",
+    image: "/colomb.webp"
+  },
+  {
+    title: "Sécurisation des données",
+    description: "Tokens d'authentifications, gestion des mots de passes, bonnes pratiques de sécurité.",
+    image: "/lockAndKey.webp"
+  },
 ];
 
 export default function GeneralSkills() {
@@ -26,11 +47,12 @@ export default function GeneralSkills() {
     >
       <div className="max-w-7xl mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 text-center">Mes compétences</h2>
-        <div className="grid gap-8 md:grid-cols-3">
+        <div className="grid gap-20 md:grid-cols-3">
           {skills.map((skill, index) => (
-            <div key={index} className="p-6 bg-white rounded-lg shadow-md">
+            <div key={index} className="p-6 bg-white rounded-lg shadow-md flex flex-col items-center text-center">
               <h3 className="text-xl font-semibold mb-2">{skill.title}</h3>
               <p className="text-gray-700">{skill.description}</p>
+              <Image src={skill.image} alt={skill.title} width={80} height={80} className="mt-auto"/>
             </div>
           ))}
         </div>
