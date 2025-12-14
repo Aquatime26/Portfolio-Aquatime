85fa279f6ea7cf9bb4a424b4c3aafa615ee07589
Alexis.Avril <alexisavril@sfr.fr>
2025-12-14 13:35:59 +0100

    fixing the next.config.ts warning message

--- my-portfolio/next.config.ts
+++ my-portfolio/next.config.ts
@@ -1,11 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   // force le mode serverless pour les API routes
+//   experimental: {
+//     serverComponentsExternalPackages: ["resend"],
+//   },
+//   // exclut les routes API du pre-rendering
+//   typescript: {
+//     ignoreBuildErrors: false,
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // force le mode serverless pour les API routes
-  experimental: {
-    serverComponentsExternalPackages: ["resend"],
-  },
-  // exclut les routes API du pre-rendering
+  serverExternalPackages: ["resend"],
   typescript: {
     ignoreBuildErrors: false,
   },
